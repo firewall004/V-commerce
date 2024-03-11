cd146423597c493c21b7e77919ce219199fc4016
Vivek Kumar <kumar97vivek@gmail.com>
2024-03-11 13:09:10 +0530

    Updated README

--- resources/views/payment-failed.blade.php
+++ resources/views/payment-failed.blade.php
@@ -8,7 +8,7 @@
                 <p class="text-lg text-center text-gray-600 mb-6">Unfortunately, your payment could not be processed at this
                     time. Please try again later or contact support for assistance.</p>
                 <div class="flex justify-center">
-                    <a href="{{ route('home') }}"
+                    <a href="/"
                         class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-md transition duration-300">Go
                         to Homepage</a>
                 </div>
